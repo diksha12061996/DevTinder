4e997025dc0bd4f617ef987ae070e86b63214d78
Deeksha Rajput <diksharajput1206@gmail.com>
2025-07-31 22:09:16 +0530

    Post,Delete, Get APIs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const app=express();
+const app = express();
 
-app.use('/test',(req,res)=>{
-    res.send("Hello from server")
-})
+app.get('/user', (req, res) => {
+    res.send({ firstName: "Deeksha", LastName: "Rajput" })
+});
+app.post('/user', (req, res) => {
+    res.send("save successfully")
+});
+app.delete('/user', (req, res) => {
+    res.send("Delete successfully")
+});
 
-app.listen(3000,()=>{
+app.use('/hello', (req, res) => {
+    res.send("Hello ")
+});
+app.use('/test', (req, res) => {
+    res.send("test from server")
+});
+
+app.listen(3000, () => {
     console.log('listening on 3000')
 });
