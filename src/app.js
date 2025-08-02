55494d48214ad1cfe28d47f45f69af08b68ae9ba
Deeksha Rajput <diksharajput1206@gmail.com>
2025-08-02 14:30:30 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -17,7 +17,19 @@ app.use('/hello', (req, res) => {
 app.use('/test', (req, res) => {
     res.send("test from server")
 });
+app.use("/RouteHandler",(req,res,next)=>{
+    res.send("Req Handler"); //only first handler will be written because of order of res.send() and if we do not retrun the handler then it will not go to the next handler to go to the second handler use next
+    next();
+},
+(req,res)=>{
+    res.send("Req Handler2");
+});
 
+app.use("/",(err,req,res,next)=>{
+if(err){
+    res.status(500).send("Something went wrong");
+}
+})
 app.listen(3000, () => {
     console.log('listening on 3000')
 });
