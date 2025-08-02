5e52d1b4a258861a750e210f99d50f552311a594
Deeksha Rajput <diksharajput1206@gmail.com>
2025-08-02 22:53:54 +0530

    Database Connection

--- src/app.js
+++ src/app.js
@@ -1,35 +1,7 @@
 const express = require('express');
 const app = express();
+require("./config/database") 
 
-app.get('/user', (req, res) => {
-    res.send({ firstName: "Deeksha", LastName: "Rajput" })
-});
-app.post('/user', (req, res) => {
-    res.send("save successfully")
-});
-app.delete('/user', (req, res) => {
-    res.send("Delete successfully")
-});
-
-app.use('/hello', (req, res) => {
-    res.send("Hello ")
-});
-app.use('/test', (req, res) => {
-    res.send("test from server")
-});
-app.use("/RouteHandler",(req,res,next)=>{
-    res.send("Req Handler"); //only first handler will be written because of order of res.send() and if we do not retrun the handler then it will not go to the next handler to go to the second handler use next
-    next();
-},
-(req,res)=>{
-    res.send("Req Handler2");
-});
-
-app.use("/",(err,req,res,next)=>{
-if(err){
-    res.status(500).send("Something went wrong");
-}
-})
 app.listen(3000, () => {
     console.log('listening on 3000')
 });
---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://diksha1206:diksha1206@nodejs.lb9f95x.mongodb.net/devTinder");
+};
+connectDB().then(() => {
+    console.log("Connected")
+}).catch((err)=>{
+    console.log("not connected")
+});
